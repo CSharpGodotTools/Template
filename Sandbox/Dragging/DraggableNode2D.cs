4e34c571021f769a7b766120daf6141fef5499f4
PiepMatz <rappersinfan@gmail.com>
2025-05-13 12:44:55 +0200

    add abstraction layer to draggables

--- Sandbox/Dragging/DraggableNode2D.cs
+++ Sandbox/Dragging/DraggableNode2D.cs
@@ -1,53 +1,31 @@
 using Godot;
 using GodotUtils;
-using System;
 
 namespace Template;
 
-public class DraggableNode2D : IDraggable
+public class DraggableNode2D(Node2D node, DraggableComponent component) 
+    : Draggable<Node2DWrapper, Node2D>(new Node2DWrapper(node), component);
+
+
+// Concrete wrapper implementation
+public class Node2DWrapper(Node2D node) : ICanvasItemWrapper<Node2D>
 {
-    public Vector2 Position { get => _node.Position; set => _node.Position = value; }
-
-    private readonly Vector2 _offset;
-    private readonly Node2D _node;
-    private readonly float _lerpFactor;
-    private readonly CollisionShape2D _collision;
-
-    public DraggableNode2D(Node2D node, DraggableComponent component)
-    {
-        _node = node;
-        _lerpFactor = component.LerpFactor;
-        _collision = node.GetNode<Area2D>(recursive: false).GetChild<CollisionShape2D>(0);
-
-        if (component.KeepOffset)
-        {
-            _offset = _node.GlobalPosition - _node.GetGlobalMousePosition();
-        }
+    public Node2D Node { get; init; } = node;
+    public Vector2 Position 
+    { 
+        get => Node.Position; 
+        set => Node.Position = value; 
     }
 
-    public void AnimateDrop(Action finished, double duration)
-    {
-        new GTween(_node)
-            .SetAnimatingProp(Node2D.PropertyName.Position)
-            .AnimateProp(Vector2.Zero, duration)
-            .Callback(finished);
+    public Vector2 GlobalPosition 
+    { 
+        get => Node.GlobalPosition; 
+        set => Node.GlobalPosition = value; 
     }
+
+    public Vector2 GetGlobalMousePosition() => 
+        Node.GetGlobalMousePosition();
     
-    public void FollowCursor()
-    {
-        // Following code might be the same as _node.GlobalPosition = _node.GlobalPosition.Lerp(_node.GetGlobalMousePosition() + _offset, _lerpFactor);
-        Vector2 target = _node.GetGlobalMousePosition() + _offset;
-        float distance = _node.GlobalPosition.DistanceTo(target);
-        _node.GlobalPosition = _node.GlobalPosition.MoveToward(target, distance * _lerpFactor);
-    }
-
-    public void Reparent(Node newParent)
-    {
-        _node.Reparent(newParent);
-    }
-
-    public void SetCollisionActive(bool active)
-    {
-        _collision.Disabled = !active;
-    }
+    public CollisionShape2D GetCollision() =>
+        Node.GetNode<Area2D>(recursive: false).GetChild<CollisionShape2D>(0);
 }
