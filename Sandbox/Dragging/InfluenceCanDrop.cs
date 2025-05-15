5e5da20229170b1bef8e72d79e01d7a005662ae9
Valk <sebastianbelle074@protonmail.com>
2025-05-15 03:16:34 +0000

    Expose Func<Node, bool> CanDrop for Dragging to Developer (#142)

---
+++ Sandbox/Dragging/InfluenceCanDrop.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+namespace Template;
+
+public partial class InfluenceCanDrop : Node
+{
+    public override void _Ready()
+    {
+        DragManager dragManager = GetParent().GetNode<DragManager>("DragManager");
+
+        dragManager.CanDropInContainer = node =>
+        {
+            if (node.Name == "Container2")
+            {
+                GD.Print("[InfluenceCanDrop.cs] (TEST) Prevent dropping any items in Container2");
+                return false;
+            }
+            
+            return true;
+        };
+    }
+}
