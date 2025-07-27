6d54ec14380d77e44432f57c288439c91d53caf7
Valk <sebastianbelle074@protonmail.com>
2025-07-27 18:40:43 -0400

    Move credits for assets to wiki

--- Framework/Scenes/MenuUI/ToolScriptHelpers/ToolScriptHelpers.cs
+++ Framework/Scenes/MenuUI/ToolScriptHelpers/ToolScriptHelpers.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using Godot;
 using GodotUtils;
 
@@ -17,61 +13,13 @@ public partial class ToolScriptHelpers : Node
         set => DeleteEmptyFolders();
     }
 
-    [Export]
-    public bool RetrieveCredits
-    {
-        get => false;
-        set => FetchAllCredits();
-    }
-
     private static void DeleteEmptyFolders()
     {
         if (!Engine.IsEditorHint()) // Do not trigger on game build
-        {
             return;
-        }
 
         DirectoryUtils.DeleteEmptyDirectories("res://");
-        GD.Print("Removed all empty folders from the project");
-    }
 
-    private static void FetchAllCredits()
-    {
-        if (!Engine.IsEditorHint()) // Do not trigger on game build
-        {
-            return;
-        }
-
-        StringBuilder creditsBuilder = new();
-
-        DirectoryUtils.Traverse("res://", ProcessCreditFile);
-
-        string finalCredits = creditsBuilder.ToString();
-
-        GD.Print(finalCredits);
-        return;
-
-        void ProcessCreditFile(string fullFilePath)
-        {
-            if (!string.Equals(Path.GetFileName(fullFilePath), "Credit.txt", StringComparison.OrdinalIgnoreCase))
-                return;
-
-            string folderParentName = new DirectoryInfo(Path.GetDirectoryName(fullFilePath) ?? string.Empty).Name;
-                
-            Dictionary<string, string> creditInfo = [];
-
-            foreach (string line in File.ReadAllLines(ProjectSettings.GlobalizePath(fullFilePath)))
-            {
-                string[] parts = line.Split(':', 2);
-
-                creditInfo[parts[0].Trim().ToLower()] = parts[1].Trim();
-            }
-
-            string authorOrArtist = creditInfo.GetValueOrDefault("artist") ?? creditInfo.GetValueOrDefault("author") ?? "Unknown";
-            string source = creditInfo.GetValueOrDefault("source") ?? "Unknown";
-            string license = creditInfo.GetValueOrDefault("license") ?? "Unknown";
-
-            creditsBuilder.AppendLine($"{folderParentName} made by {authorOrArtist} LICENSED {license}: {source}");
-        }
+        GD.Print("Removed all empty folders from the project. Restart the game engine or wait some time to see the effect.");
     }
 }
