77ba0eb4c43d00bb501fb78b6b4d00eabb152e20
Valk <sebastianbelle074@protonmail.com>
2025-07-29 13:32:48 -0400

    Improve readability of AudioManager.cs

--- Framework/Autoloads/AudioManager/AudioManager.cs
+++ Framework/Autoloads/AudioManager/AudioManager.cs
@@ -5,10 +5,16 @@ namespace __TEMPLATE__;
 
 public partial class AudioManager : Node
 {
+    private const float MinRandomPitch        = 0.8f;
+    private const float MaxRandomPitch        = 1.2f;
+    private const float RandomPitchThreshold  = 0.1f;
+    private const int   MutedVolume           = -80;
+    private const int   MutedVolumeNormalized = -40;
+
     private GAudioPlayer    _musicPlayer;
+    private ResourceOptions _options;
     private Node            _sfxPlayersParent;
     private float           _lastPitch;
-    private ResourceOptions _options;
 
     public override void _Ready()
     {
@@ -17,38 +23,19 @@ public partial class AudioManager : Node
 
         _sfxPlayersParent = new Node();
         AddChild(_sfxPlayersParent);
-
-        //PlayMusic(Music.Menu);
     }
 
     public void PlayMusic(AudioStream song, bool instant = true, double fadeOut = 1.5, double fadeIn = 0.5)
     {
         if (!instant && _musicPlayer.Playing)
         {
-            float volume = _options.MusicVolume;
-            float volumeRemapped =
-                volume == 0 ? -80 : volume.Remap(0, 100, -40, 0);
-
-            // Transition from current song being played to new song
-            new GTween(_musicPlayer.Internal)
-                .SetAnimatingProp(AudioStreamPlayer.PropertyName.VolumeDb)
-                // Fade out current song
-                .AnimateProp(-80, fadeOut).EaseIn()
-                // Set to new song
-                .Callback(() =>
-                {
-                    _musicPlayer.Stream = song;
-                    _musicPlayer.Play();
-                })
-                // Fade in to current song
-                .AnimateProp(volumeRemapped, fadeIn).EaseIn();
+            // Slowly transition to the new song
+            PlayAudioCrossfade(_musicPlayer, song, _options.MusicVolume, fadeOut, fadeIn);
         }
         else
         {
-            // Instantly switch to and play new song
-            _musicPlayer.Stream = song;
-            _musicPlayer.Volume = _options.MusicVolume;
-            _musicPlayer.Play();
+            // Instantly switch to the new song
+            PlayAudio(_musicPlayer, song, _options.MusicVolume);
         }
     }
 
@@ -63,14 +50,16 @@ public partial class AudioManager : Node
 
         // Randomize the pitch
         RandomNumberGenerator rng = new();
+
         rng.Randomize();
-        float pitch = rng.RandfRange(0.8f, 1.2f);
+
+        float pitch = rng.RandfRange(MinRandomPitch, MaxRandomPitch);
 
         // Ensure the current pitch is not the same as the last
-        while (Mathf.Abs(pitch - _lastPitch) < 0.1f)
+        while (Mathf.Abs(pitch - _lastPitch) < RandomPitchThreshold)
         {
             rng.Randomize();
-            pitch = rng.RandfRange(0.8f, 1.2f);
+            pitch = rng.RandfRange(MinRandomPitch, MaxRandomPitch);
         }
 
         _lastPitch = pitch;
@@ -87,8 +76,7 @@ public partial class AudioManager : Node
     {
         foreach (AudioStreamPlayer audioPlayer in _sfxPlayersParent.GetChildren<AudioStreamPlayer>())
         {
-            new GTween(audioPlayer)
-                .Animate(AudioStreamPlayer.PropertyName.VolumeDb, -80, fadeTime);
+            new GTween(audioPlayer).Animate(AudioStreamPlayer.PropertyName.VolumeDb, MutedVolume, fadeTime);
         }
     }
 
@@ -104,12 +92,43 @@ public partial class AudioManager : Node
         _options.SFXVolume = v;
 
         // Can't cast to GAudioPlayer so will have to remap manually again
-        v = v == 0 ? -80 : v.Remap(0, 100, -40, 0);
+        v = NormalizeConfigVolume(v);
 
         // Set volume of all SFX players currently in the scene
         foreach (AudioStreamPlayer audioPlayer in _sfxPlayersParent.GetChildren())
         {
             audioPlayer.VolumeDb = v;
         }
+    }
+
+    private static void PlayAudio(GAudioPlayer player, AudioStream song)
+    {
+        player.Stream = song;
+        player.Play();
+    }
+
+    private static void PlayAudio(GAudioPlayer player, AudioStream song, float volume)
+    {
+        player.Stream = song;
+        player.Volume = volume;
+        player.Play();
+    }
+
+    private static void PlayAudioCrossfade(GAudioPlayer player, AudioStream song, float volume, double fadeOut, double fadeIn)
+    {
+        // Transition from current song being played to new song
+        new GTween(player.Internal)
+            .SetAnimatingProp(AudioStreamPlayer.PropertyName.VolumeDb)
+            // Fade out current song
+            .AnimateProp(MutedVolume, fadeOut).EaseIn()
+            // Set to new song
+            .Callback(() => PlayAudio(player, song))
+            // Fade in to current song
+            .AnimateProp(NormalizeConfigVolume(volume), fadeIn).EaseIn();
+    }
+
+    private static float NormalizeConfigVolume(float volume)
+    {
+        return volume == 0 ? MutedVolume : volume.Remap(0, 100, MutedVolumeNormalized, 0);
     }
 }
