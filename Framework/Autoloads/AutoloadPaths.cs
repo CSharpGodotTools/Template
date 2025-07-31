e8c44eb0c2ee65380ce2e00bab1f03ad1b575cf8
Valk <sebastianbelle074@protonmail.com>
2025-07-31 19:58:17 -0400

    hotfix: Fix incorrect autoload paths

--- Framework/Autoloads/AutoloadPaths.cs
+++ Framework/Autoloads/AutoloadPaths.cs
@@ -3,12 +3,12 @@ namespace __TEMPLATE__;
 public static class AutoloadPaths
 {
     public const string Global         = BasePath; 
-    public const string AudioManager   = BasePath + "AudioManager";
-    public const string Console        = BasePath + "Debug/Console";
-    public const string MetricsOverlay = BasePath + "Debug/MetricsOverlay";
-    public const string OptionsManager = BasePath + "OptionsManager";
-    public const string SceneManager   = BasePath + "SceneManager";
-    public const string Services       = BasePath + "Services";
+    public const string AudioManager   = BasePath + "ComponentManager/AudioManager";
+    public const string Console        = BasePath + "ComponentManager/Debug/Console";
+    public const string MetricsOverlay = BasePath + "ComponentManager/Debug/MetricsOverlay";
+    public const string OptionsManager = BasePath + "ComponentManager/OptionsManager";
+    public const string SceneManager   = BasePath + "ComponentManager/SceneManager";
+    public const string Services       = BasePath + "ComponentManager/Services";
 
-    private const string BasePath = "/root/Autoloads/ComponentManager/";
+    private const string BasePath = "/root/Autoloads/";
 }
