cd7d7c8792f3c22ee6a5b747cf47c8cb34b40163
Valk <sebastianbelle074@protonmail.com>
2025-12-01 18:48:45 -0600

    Remove singleton pattern from GameConsole

--- Framework/Console/GameConsole.cs
+++ Framework/Console/GameConsole.cs
@@ -9,8 +9,6 @@ public partial class GameConsole : Node
 {
     private const int MaxTextFeed = 1000;
 
-    public static GameConsole Instance { get; private set; }
-
     private List<ConsoleCommandInfo> _commands = [];
     private ConsoleHistory           _history = new();
     private PanelContainer           _mainContainer;
@@ -23,11 +21,6 @@ public partial class GameConsole : Node
 
     public override void _Ready()
     {
-        if (Instance != null)
-            throw new InvalidOperationException($"{nameof(GameConsole)} was initialized already");
-
-        Instance = this;
-
         _feed          = GetNode<TextEdit>("%Output");
         _input         = GetNode<LineEdit>("%CmdsInput");
         _settingsBtn   = GetNode<Button>("%Settings");
@@ -60,8 +53,6 @@ public partial class GameConsole : Node
         _input.TextSubmitted -= OnConsoleInputEntered;
         _settingsBtn.Pressed -= OnSettingsBtnPressed;
         _settingsAutoScroll.Toggled -= OnAutoScrollToggeled;
-
-        Instance = null;
     }
 
     public List<ConsoleCommandInfo> GetCommands()
@@ -69,7 +60,7 @@ public partial class GameConsole : Node
         return _commands;
     }
 
-    public static ConsoleCommandInfo RegisterCommand(string cmd, Action<string[]> code)
+    public ConsoleCommandInfo RegisterCommand(string cmd, Action<string[]> code)
     {
         ConsoleCommandInfo info = new()
         {
@@ -77,7 +68,7 @@ public partial class GameConsole : Node
             Code = code
         };
 
-        Instance._commands.Add(info);
+        _commands.Add(info);
 
         return info;
     }
@@ -106,16 +97,16 @@ public partial class GameConsole : Node
         ScrollDown();
     }
 
-    public static bool Visible => Instance._mainContainer.Visible;
+    public bool Visible => _mainContainer.Visible;
 
-    public static void ToggleVisibility()
+    public void ToggleVisibility()
     {
-        Instance._mainContainer.Visible = !Instance._mainContainer.Visible;
+        _mainContainer.Visible = !_mainContainer.Visible;
 
-        if (Instance._mainContainer.Visible)
+        if (_mainContainer.Visible)
         {
-            Instance._input.GrabFocus();
-            Instance.CallDeferred(nameof(ScrollDown));
+            _input.GrabFocus();
+            CallDeferred(nameof(ScrollDown));
         }
     }
 
