cd349964ae96880a9a74c8e8f94be6072f66b207
Valk <sebastianbelle074@protonmail.com>
2025-12-03 15:13:13 -0600

    Add extensive error checking to Game.cs

--- Framework/Game.cs
+++ Framework/Game.cs
@@ -1,11 +1,49 @@
 ﻿using __TEMPLATE__.Debugging;
 using __TEMPLATE__.UI;
 using __TEMPLATE__.UI.Console;
+using System;
 
 namespace __TEMPLATE__;
 
 public static class Game
 {
+#if DEBUG
+    public static MetricsOverlay Metrics => IsAutoloadsSetup(a => a.MetricsOverlay, nameof(Metrics));
+    public static OptionsManager Options => IsAutoloadsSetup(a => a.OptionsManager, nameof(Options));
+    public static AudioManager Audio => IsAutoloadsSetup(a => a.AudioManager, nameof(Audio));
+    public static SceneManager Scene => IsAutoloadsSetup(a => a.SceneManager, nameof(Scene));
+    public static GameConsole Console => IsAutoloadsSetup(a => a.GameConsole, nameof(Console));
+    public static Profiler Profiler => IsAutoloadsSetup(a => a.Profiler, nameof(Profiler));
+    public static Services Services => IsAutoloadsSetup(a => a.Services, nameof(Services));
+    public static Logger Logger => IsAutoloadsSetup(a => a.Logger, nameof(Logger));
+
+    /// <summary>
+    /// Check if the autoloads singleton is not null. If it is null then show an error explaining
+    /// that the developer cannot access the autoloads singleton just yet and it needs time to
+    /// setup.
+    /// </summary>
+    private static T IsAutoloadsSetup<T>(Func<Autoloads, T> getPropertyFrom, string propertyName) where T : class
+    {
+        Autoloads autoloads = Autoloads.Instance;
+
+        if (autoloads == null)
+        {
+            string errMsg = $"Game.{propertyName} was accessed before _EnterTree or _Ready.";
+
+#if NETCODE_ENABLED
+            // Show a friendly optional error message if netcode is enabled.
+            // Since Autoloads is null we can make our own temporary Logger.
+            Logger logger = new();
+            logger.LogDebug(errMsg + " (see exception in errors for stack trace)");
+            logger.Update();
+#endif
+            throw new InvalidOperationException(errMsg);
+        }
+
+        return getPropertyFrom(autoloads)!; // Assumes the field may be null, but we are not checking it here
+    }
+#else
+    // The games release will not have the slow debugging checks
     public static MetricsOverlay Metrics  => Autoloads.Instance.MetricsOverlay;
     public static OptionsManager Options  => Autoloads.Instance.OptionsManager;
     public static AudioManager   Audio    => Autoloads.Instance.AudioManager;
@@ -14,4 +52,5 @@ public static class Game
     public static Profiler       Profiler => Autoloads.Instance.Profiler;
     public static Services       Services => Autoloads.Instance.Services;
     public static Logger         Logger   => Autoloads.Instance.Logger;
-}
+#endif
+        }
