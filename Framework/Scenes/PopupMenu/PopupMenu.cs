1d202cd0fe658a3b70e15b8b8e73a244dda7df76
Valk <sebastianbelle074@protonmail.com>
2026-01-11 13:06:31 -0600

    Add #regions to PopupMenu.cs

--- Framework/Scenes/PopupMenu/PopupMenu.cs
+++ Framework/Scenes/PopupMenu/PopupMenu.cs
@@ -9,38 +9,39 @@ namespace __TEMPLATE__.UI;
 
 public partial class PopupMenu : Control
 {
+    #region Exports
     [Export] private PackedScene _optionsPrefab;
-    [Export] private Button _resumeBtn;
-    [Export] private Button _restartBtn;
-    [Export] private Button _optionsBtn;
-    [Export] private Button _mainMenuBtn;
-    [Export] private Button _quitBtn;
+    #endregion
 
+    #region Events
     public event Action Opened;
     public event Action Closed;
     public event Action OptionsOpened;
     public event Action OptionsClosed;
     public event Action MainMenuBtnPressed;
+    #endregion
 
-    private GameConsole _console;
-    private PanelContainer _menu;
+    #region Nodes
+    private Button _resumeBtn;
+    private Button _restartBtn;
+    private Button _optionsBtn;
+    private Button _mainMenuBtn;
+    private Button _quitBtn;
+
     private VBoxContainer _nav;
+    private GameConsole _console;
     private Options _options;
+    private Control _menu;
+    #endregion   
 
+    #region Godot Overrides
     public override void _Ready()
     {
-        _console = Game.Console;
-        _menu = GetNode<PanelContainer>("%Menu");
-        _nav = GetNode<VBoxContainer>("%Navigation");
+        ProcessMode = ProcessModeEnum.Always;
+        InitializeNodes();
+        RegisterNodeEvents();
+        RegisterGlobalHandlers();
 
-        _resumeBtn.Pressed += OnResumePressed;
-        _restartBtn.Pressed += OnRestartPressed;
-        _optionsBtn.Pressed += OnOptionsPressed;
-        _mainMenuBtn.Pressed += OnMainMenuPressed;
-        _quitBtn.Pressed += OnQuitPressed;
-
-        Game.Services.Register(this);
-        Game.FocusOutline.RegisterPopupMenu(this);
         CreateOptions();
         HideOptions();
         Hide();
@@ -69,15 +70,106 @@ public partial class PopupMenu : Control
 
     public override void _ExitTree()
     {
+        UnregisterNodeEvents();
+        UnregisterGlobalHandlers();
+    }
+    #endregion
+
+    #region Initialization
+    private void InitializeNodes()
+    {
+        _console = Game.Console;
+        _nav = GetNode<VBoxContainer>("%Navigation");
+        _menu = GetNode<Control>("Menu");
+
+        _resumeBtn = _nav.GetNode<Button>("Resume");
+        _restartBtn = _nav.GetNode<Button>("Restart");
+        _optionsBtn = _nav.GetNode<Button>("Options");
+        _mainMenuBtn = _nav.GetNode<Button>("Main Menu");
+        _quitBtn = _nav.GetNode<Button>("Quit");
+    }
+
+    private void RegisterNodeEvents()
+    {
+        _resumeBtn.Pressed += OnResumePressed;
+        _restartBtn.Pressed += OnRestartPressed;
+        _optionsBtn.Pressed += OnOptionsPressed;
+        _mainMenuBtn.Pressed += OnMainMenuPressed;
+        _quitBtn.Pressed += OnQuitPressed;
+    }
+
+    private void UnregisterNodeEvents()
+    {
         _resumeBtn.Pressed -= OnResumePressed;
         _restartBtn.Pressed -= OnRestartPressed;
         _optionsBtn.Pressed -= OnOptionsPressed;
         _mainMenuBtn.Pressed -= OnMainMenuPressed;
         _quitBtn.Pressed -= OnQuitPressed;
-
-        Game.FocusOutline.UnregisterPopupMenu(this);
     }
 
+    private void RegisterGlobalHandlers()
+    {
+        Game.Services.Register(this);
+        Game.FocusOutline.RegisterPopupMenu(this);
+    }
+
+    private void UnregisterGlobalHandlers()
+    {
+        Game.FocusOutline.UnregisterPopupMenu(this);
+    }
+    #endregion
+
+    #region Popup Menu
+    private void CreateOptions()
+    {
+        _options = _optionsPrefab.Instantiate<Options>();
+        AddChild(_options);
+    }
+
+    private void ShowOptions()
+    {
+        _options.ProcessMode = ProcessModeEnum.Always;
+        _options.Show();
+        OptionsOpened?.Invoke();
+    }
+
+    private void HideOptions()
+    {
+        _options.ProcessMode = ProcessModeEnum.Disabled;
+        _options.Hide();
+        OptionsClosed?.Invoke();
+        FocusResumeBtn();
+    }
+
+    private void ToggleGamePause()
+    {
+        if (Visible)
+            ResumeGame();
+        else
+            PauseGame();
+    }
+
+    private void PauseGame()
+    {
+        Visible = true;
+        GetTree().Paused = true;
+        Opened?.Invoke();
+        FocusResumeBtn();
+    }
+
+    private void ResumeGame()
+    {
+        Visible = false;
+        GetTree().Paused = false;
+        Closed?.Invoke();
+    }
+
+    private void FocusResumeBtn() => _resumeBtn.GrabFocus();
+    private void ShowPopupMenu() => _menu.Show();
+    private void HidePopupMenu() => _menu.Hide();
+    #endregion
+
+    #region Subscribers
     private void OnResumePressed()
     {
         Hide();
@@ -108,57 +200,5 @@ public partial class PopupMenu : Control
     {
         BaseAutoloads.Instance.ExitGame().FireAndForget();
     }
-
-    private void CreateOptions()
-    {
-        _options = _optionsPrefab.Instantiate<Options>();
-        AddChild(_options);
-    }
-
-    private void ShowOptions()
-    {
-        _options.ProcessMode = ProcessModeEnum.Always;
-        _options.Show();
-        OptionsOpened?.Invoke();
-    }
-
-    private void HideOptions()
-    {
-        _options.ProcessMode = ProcessModeEnum.Disabled;
-        _options.Hide();
-        OptionsClosed?.Invoke();
-        FocusResumeBtn();
-    }
-
-    private void ShowPopupMenu()
-    {
-        _menu.Show();
-    }
-
-    private void HidePopupMenu() => _menu.Hide();
-
-    private void ToggleGamePause()
-    {
-        if (Visible)
-            ResumeGame();
-        else
-            PauseGame();
-    }
-
-    private void FocusResumeBtn() => GetNode<Button>("%Resume").GrabFocus();
-
-    private void PauseGame()
-    {
-        Visible = true;
-        GetTree().Paused = true;
-        Opened?.Invoke();
-        FocusResumeBtn();
-    }
-
-    private void ResumeGame()
-    {
-        Visible = false;
-        GetTree().Paused = false;
-        Closed?.Invoke();
-    }
+    #endregion
 }
