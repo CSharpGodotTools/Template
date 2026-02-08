79d7eda425dd10c559ae6cdec5a15725649a2431
Valk <sebastianbelle074@protonmail.com>
2026-02-08 03:48:21 -0600

    Add ServerClientConnects test

--- Setup/Testing/Tests.cs
+++ Setup/Testing/Tests.cs
@@ -1,5 +1,11 @@
 ﻿using GdUnit4;
 using static GdUnit4.Assertions;
+using Framework.Netcode;
+using Framework.Netcode.Examples.Topdown;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Framework;
 
 namespace Template.Setup.Testing;
 
@@ -10,5 +16,45 @@ public class Tests
     public static void StringToLower()
     {
         AssertString("AbcD".ToLower()).IsEqual("abcd");
+    }
+
+    [TestCase]
+    public static async Task ServerClientConnects()
+    {
+        Logger.UseGodotBackend = false;
+        GameFramework.UseLoggerForTests(new Logger());
+
+        GameServer server = new();
+        GameClient client = new();
+
+        server.Start(25565, 100, new ENetOptions());
+
+        Task connectTask = client.Connect("127.0.0.1", 25565, new ENetOptions());
+
+        try
+        {
+            bool connected = false;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            while (stopwatch.Elapsed < TimeSpan.FromSeconds(2))
+            {
+                if (client.IsConnected)
+                {
+                    connected = true;
+                    break;
+                }
+
+                await Task.Delay(10);
+            }
+
+            AssertBool(connected).IsTrue();
+        }
+        finally
+        {
+            client.Stop();
+            server.Stop();
+            await connectTask;
+            GameFramework.ClearLoggerOverride();
+        }
     }
 }
