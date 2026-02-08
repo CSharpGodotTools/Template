898b56ff0880e9800fc3e00a83e0b40a437e191b
Valk <sebastianbelle074@protonmail.com>
2026-02-08 02:55:28 -0600

    Setup gdUnit4
    Further setup instructions will be needed

---
+++ Setup/Testing/Tests.cs
@@ -0,0 +1,14 @@
+﻿using GdUnit4;
+using static GdUnit4.Assertions;
+
+namespace Template.Setup.Testing;
+
+[TestSuite]
+public class Tests
+{
+    [TestCase]
+    public static void StringToLower()
+    {
+        AssertString("AbcD".ToLower()).IsEqual("abcd");
+    }
+}
