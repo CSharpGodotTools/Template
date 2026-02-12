c343f9bdc7a8001eddee42d4f53271371c0e918c
Valk <sebastianbelle074@protonmail.com>
2026-02-12 14:38:37 -0600

    Add error feedback to game name LineEdit

--- addons/template_setup_plugin/Scripts/TemplateSetupDock.cs
+++ addons/template_setup_plugin/Scripts/TemplateSetupDock.cs
@@ -1,40 +1,109 @@
 #if TOOLS
+using Framework.Setup;
 using Godot;
 using GodotUtils;
 
 [Tool]
 public partial class TemplateSetupDock : VBoxContainer
 {
-    private Label _gameNamePreview;
+    private const double FeedbackResetTime = 2.0;
+
     private LineEdit _projectNameEdit;
+    private Button _applyButton;
+    private Label _gameNamePreview;
+    private Timer _feedbackResetTimer;
+    private string _prevGameName = "";
 
     public override void _Ready()
     {
+        AddChild(_feedbackResetTimer = new Timer());
+        _feedbackResetTimer.Timeout += OnFeedbackResetTimerTimeout;
+
         MarginContainer margin = MarginContainerFactory.Create(30);
+
+        VBoxContainer vbox = new();
+
+        vbox.AddChild(_gameNamePreview = new()
+        {
+            SizeFlagsHorizontal = SizeFlags.ShrinkCenter
+        });
+
         HBoxContainer hbox = new();
 
         hbox.AddChild(new Label { Text = "Project Name:" });
 
-        _projectNameEdit = new()
+        hbox.AddChild(_projectNameEdit = new()
         {
             SizeFlagsHorizontal = SizeFlags.ShrinkBegin,
             CustomMinimumSize = new Vector2(200, 0)
-        };
+        });
 
-        hbox.AddChild(_projectNameEdit);
-        margin.AddChild(hbox);
+        _projectNameEdit.TextChanged += OnProjectNameChanged;
+
+        vbox.AddChild(hbox);
+
+        margin.AddChild(vbox);
 
         AddChild(margin);
 
-        Button applyButton = new()
+        _applyButton = new Button()
         {
             Text = "Apply Setup",
             SizeFlagsHorizontal = SizeFlags.ShrinkCenter,
             CustomMinimumSize = new Vector2(200, 0)
         };
 
-        applyButton.Pressed += OnApplyPressed;
-        AddChild(applyButton);
+        _applyButton.Pressed += OnApplyPressed;
+        AddChild(_applyButton);
+    }
+
+    public override void _ExitTree()
+    {
+        _feedbackResetTimer.Timeout -= OnFeedbackResetTimerTimeout;
+        _applyButton.Pressed -= OnApplyPressed;
+    }
+
+    private void OnFeedbackResetTimerTimeout()
+    {
+        _gameNamePreview.Text = _prevGameName;
+    }
+
+    private void OnProjectNameChanged(string gameName)
+    {
+        _feedbackResetTimer.Stop();
+
+        if (string.IsNullOrWhiteSpace(gameName))
+        {
+            _gameNamePreview.Text = "";
+            _prevGameName = "";
+            return;
+        }
+
+        if (char.IsNumber(gameName.Trim()[0]))
+        {
+            _gameNamePreview.Text = "The first character cannot be a number";
+            _feedbackResetTimer.Start(FeedbackResetTime);
+            ResetNameEdit();
+            return;
+        }
+
+        if (!SetupUtils.IsAlphaNumericAndAllowSpaces(gameName))
+        {
+            _gameNamePreview.Text = "Special characters are not allowed";
+            _feedbackResetTimer.Start(FeedbackResetTime);
+            ResetNameEdit();
+            return;
+        }
+
+        _gameNamePreview.Text = gameName;
+        _prevGameName = gameName;
+        return;
+
+        void ResetNameEdit()
+        {
+            _projectNameEdit.Text = _prevGameName;
+            _projectNameEdit.CaretColumn = _prevGameName.Length;
+        }
     }
 
     private void OnApplyPressed()
