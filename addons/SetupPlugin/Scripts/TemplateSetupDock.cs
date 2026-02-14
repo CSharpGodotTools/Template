12173e45ae75a105509d1ffc52d28e05f7ed5ac4
Valk <sebastianbelle074@protonmail.com>
2026-02-14 04:06:00 -0600

    add comments to setup dock script

--- addons/SetupPlugin/Scripts/TemplateSetupDock.cs
+++ addons/SetupPlugin/Scripts/TemplateSetupDock.cs
@@ -15,9 +15,12 @@ public partial class TemplateSetupDock : VBoxContainer
     private const string SetupPluginName = "SetupPlugin";
     private const string MainSceneName = "Level";
     private const string DefaultClearColorPath = "rendering/environment/defaults/default_clear_color";
+    private const string MainScenePath = "application/run/main_scene";
     private const int Padding = 120;
     private const string MainScenesPath = "res://addons/SetupPlugin/MainScenes/";
 
+    // Not really sure what to call this. The key can be either "2D" or "3D" and the list can be like ["Minimal"] or ["Minimal", "FPS"]
+    // Also these fields are kind of clumped together. Maybe need to group / organize them.
     Dictionary<string, List<string>> _sceneTypes = new();
     private ConfirmationDialog _confirmRestartDialog;
     private GameNameValidator _gameNameValidator;
@@ -28,11 +31,14 @@ public partial class TemplateSetupDock : VBoxContainer
     private VBoxContainer _vbox;
     private Label _gameNamePreview;
     private string _prevGameName = "";
-    private string _projectTypeStr;
-    private string _templateTypeStr;
+    private string _projectTypeStr; // not really sure what to call this
+    private string _templateTypeStr; // not really sure what to call this
 
     public override void _Ready()
     {
+        // There is a lot going on in this _Ready() method and I'm not really sure what kind of OOP should be done here
+        // without making it too hard to expand on later
+
         // Restart dialog
         _confirmRestartDialog = new ConfirmationDialog
         {
@@ -67,10 +73,12 @@ public partial class TemplateSetupDock : VBoxContainer
 
         _projectType = new();
 
+        // Traverse the specific folders "2D" and "3D" (more could be added in the future like "VR" as an example I guess idk
         DirectoryUtils.Traverse(ProjectSettings.GlobalizePath(MainScenesPath), templateTypeEntry =>
         {
             _sceneTypes[templateTypeEntry.FileName] = new List<string>();
 
+            // Traverse the specific template folder types
             DirectoryUtils.Traverse(ProjectSettings.GlobalizePath(Path.Combine(MainScenesPath, templateTypeEntry.FileName)), entry =>
             {
                 // Add specific template types "Minimal" and "FPS"
@@ -126,6 +134,7 @@ public partial class TemplateSetupDock : VBoxContainer
         MarginContainer margin = MarginContainerFactory.Create(30);
         _vbox = new VBoxContainer();
 
+        // Actually start adding stuff to the tree now
         AddChild(feedbackResetTimer);
 
         _vbox.AddChild(_gameNamePreview);
@@ -143,6 +152,9 @@ public partial class TemplateSetupDock : VBoxContainer
         EditorInterface.Singleton.GetEditorMainScreen().AddChild(_confirmRestartDialog);
     }
 
+    /// <summary>
+    /// Pairs a label with <paramref name="text"/> next to a <paramref name="control"/> using a specific padding and right alignment.
+    /// </summary>
     private void AddLabelH(string text, Control control)
     {
         HBoxContainer hbox = new();
@@ -156,14 +168,19 @@ public partial class TemplateSetupDock : VBoxContainer
         _vbox.AddChild(hbox);
     }
 
+    /// <summary>
+    /// The project type that was selected ("2D" / "3D")
+    /// </summary>
     private void OnProjectTypeSelected(long index)
     {
+        // Clear template types
         _templateType.Clear();
 
         string projectType = _projectType.GetItemText((int)index);
         _projectTypeStr = projectType;
         //GD.Print("Project type was changed to " + projectType);
 
+        // Re-populate template types depending on what project type was selected
         foreach (string templateType in _sceneTypes[projectType])
         {
             _templateType.AddItem(templateType);
@@ -173,6 +190,9 @@ public partial class TemplateSetupDock : VBoxContainer
         //GD.Print("Template type was changed to " + _templateTypeStr);
     }
 
+    /// <summary>
+    /// The template type that was selected ("Minimal", "FPS")
+    /// </summary>
     private void OnTemplateTypeSelected(long index)
     {
         _templateTypeStr = _templateType.GetItemText((int)index);
@@ -208,6 +228,7 @@ public partial class TemplateSetupDock : VBoxContainer
         _confirmRestartDialog.PopupCentered();
     }
 
+    // This should probably go in its own file
     private class ProjectSetup
     {
         private readonly string _projectRoot;
@@ -223,12 +244,16 @@ public partial class TemplateSetupDock : VBoxContainer
             DirectoryUtils.DeleteEmptyDirectories(_projectRoot);
 
             // Move the appropriate template files to root
+            // This Path.Combine is really long but I'm afraid to do a/b/c because I remember doing this broke on linux systems (but works fine on windows)
             string templateFolder = Path.Combine(_projectRoot, "addons", "SetupPlugin", "MainScenes", projectType, templateType);
 
             //Console.WriteLine("Setup ran with following settings:");
             //Console.WriteLine(projectType);
             //Console.WriteLine(templateType);
 
+            // Copy all files and folders from the respective template folder to root
+            // Maybe DirectoryUtils could have a method that copies files from one folder to another that preserves file structure like 
+            // what is being done here, but what would such a method be called?
             DirectoryUtils.Traverse(templateFolder, entry =>
             {
                 string relativePath = Path.GetRelativePath(templateFolder, entry.FullPath);
@@ -247,16 +272,15 @@ public partial class TemplateSetupDock : VBoxContainer
                 return TraverseDecision.Continue;
             });
 
-            ProjectSettings.SetSetting("application/run/main_scene", "res://Level.tscn");
-            ProjectSettings.Save();
+            ProjectSettings.SetSetting(MainScenePath, "res://Level.tscn");
+            ProjectSettings.Save(); // This has to be set before SetupUtils.RenameProjectFiles(...) or assembly wont get set for some reason
 
-            SetupUtils.RenameProjectFiles(_projectRoot, formattedGameName);
-            SetupUtils.RenameAllNamespaces(_projectRoot, formattedGameName);
+            SetupUtils.RenameProjectFiles(_projectRoot, formattedGameName); // This needs to be set after ProjectSettings.Save() or assembly wont get set for some reason
+            SetupUtils.RenameAllNamespaces(_projectRoot, formattedGameName); // We rename all namespaces after all files have been moved / copied
             SetupUtils.EnsureGDIgnoreFilesInGDUnitTestFolders(_projectRoot);
 
             // After the editor restarts the following errors and warnigns will appear and can safely be ignored:
             // WARNING: editor/editor_node.cpp:4320 - Addon 'res://addons/SetupPlugin/plugin.cfg' failed to load. No directory found. Removing from enabled plugins.
-            // ERROR: Cannot navigate to 'res://addons/SetupPlugin/' as it has not been found in the file system!
             DeleteSetupPlugin();
             SaveActiveScene();
             RestartEditor();
