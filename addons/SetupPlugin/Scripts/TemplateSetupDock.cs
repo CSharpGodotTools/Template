f5f2252c26e9fe77a677c39e55211fd60572ba05
Valk <sebastianbelle074@protonmail.com>
2026-02-14 02:57:59 -0600

    Move game name validator to its own file

--- addons/SetupPlugin/Scripts/TemplateSetupDock.cs
+++ addons/SetupPlugin/Scripts/TemplateSetupDock.cs
@@ -12,7 +12,6 @@ namespace Framework.Setup;
 [Tool]
 public partial class TemplateSetupDock : VBoxContainer
 {
-    private const double FeedbackResetTime = 2.0;
     private const string SetupPluginName = "SetupPlugin";
     private const string MainSceneName = "Level";
     private const string DefaultClearColorPath = "rendering/environment/defaults/default_clear_color";
@@ -260,49 +259,6 @@ public partial class TemplateSetupDock : VBoxContainer
         private void RestartEditor()
         {
             EditorInterface.Singleton.RestartEditor(save: false);
-        }
-    }
-
-    private class GameNameValidator(Label gameNamePreview, Timer feedbackResetTimer, LineEdit projectNameEdit)
-    {
-        private string _prevGameName = "";
-
-        public void Validate(string gameName)
-        {
-            feedbackResetTimer.Stop();
-
-            if (string.IsNullOrWhiteSpace(gameName))
-            {
-                gameNamePreview.Text = "";
-                _prevGameName = "";
-                return;
-            }
-
-            if (char.IsNumber(gameName.Trim()[0]))
-            {
-                gameNamePreview.Text = "The first character cannot be a number";
-                feedbackResetTimer.Start(FeedbackResetTime);
-                ResetNameEdit();
-                return;
-            }
-
-            if (!SetupUtils.IsAlphaNumericAndAllowSpaces(gameName))
-            {
-                gameNamePreview.Text = "Special characters are not allowed";
-                feedbackResetTimer.Start(FeedbackResetTime);
-                ResetNameEdit();
-                return;
-            }
-
-            gameNamePreview.Text = SetupUtils.FormatGameName(gameName);
-            _prevGameName = gameName;
-            return;
-
-            void ResetNameEdit()
-            {
-                projectNameEdit.Text = _prevGameName;
-                projectNameEdit.CaretColumn = _prevGameName.Length;
-            }
         }
     }
 }
