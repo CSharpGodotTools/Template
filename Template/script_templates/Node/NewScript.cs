6338683ee4d5ba97d3ad022e203661b4a5f7967d
Tom Pohorily <sebastianbelle074@gmail.com>
2026-03-14 13:57:41 -0500

    Add custom defined script template

---
+++ Template/script_templates/Node/NewScript.cs
@@ -0,0 +1,16 @@
+// meta-name: Minimal script
+// meta-description: Minimal script
+// meta-default: true
+// meta-space-indent: 4
+
+using _BINDINGS_NAMESPACE_;
+
+namespace __TEMPLATE__;
+
+public partial class _CLASS_ : _BASE_
+{
+    public override void _Ready()
+    {
+        
+    }
+}
