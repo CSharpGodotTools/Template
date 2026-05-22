3878b651abe6d4b79a0cd4bcedbd06cef1d00559
Tom Pohorily <sebastianbelle074@gmail.com>
2026-05-22 15:29:51 -0500

    refactor: decouple Debugger+ from SetupPlugin

--- Template/addons/DebuggerPlus/Scripts/Dock/DebuggerErrorFormatter.gd
+++ Template/addons/DebuggerPlus/Scripts/Dock/DebuggerErrorFormatter.gd
@@ -1,4 +1,4 @@
 # Exposes DebuggerErrorFormatter as a globally accessible class name.
 # The full implementation lives in Scripts/Dock/Debugger/DebuggerErrorFormatter.gd.
 class_name DebuggerErrorFormatter
-extends "res://addons/SetupPlugin/Scripts/Dock/Debugger/DebuggerErrorFormatter.gd"
+extends "res://addons/DebuggerPlus/Scripts/Dock/Debugger/DebuggerErrorFormatter.gd"
